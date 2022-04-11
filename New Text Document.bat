0dba33562067114a621ea575d1a668c2eec29642
Benyamin-Vala <102605148+Benyamin-Vala@users.noreply.github.com>
2022-04-11 18:17:58 +0300

    Delete New Text Document.bat

--- New Text Document.bat
+++
@@ -1,7 +0,0 @@
-echo "Password Generator With Python" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Benyamin-Vala/Password-Generator.git
-git push -u origin main
